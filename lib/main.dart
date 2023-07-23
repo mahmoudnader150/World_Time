91d469d61d0cd2c2829940084da27d134193e50f
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-07-24 01:12:56 +0300

    Routing between pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
 
 void main() {
-  runApp(const MaterialApp(
-         home:Home()
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => const Loading(),
+      '/home': (context) => const Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
-
