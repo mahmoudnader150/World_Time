8ff2fa8c45800bb443684ff9f32f902f1ee740f3
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-07-24 14:28:55 +0300

    Build dummy async function

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -10,8 +10,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+   void getData() async{
+     // simulate netwrok request for a username
+     String username = await Future.delayed(Duration(seconds: 3),(){
+       return 'Mahmoud';
+     });
+
+     // simulate netwrok request to get bio for username
+     String bio = await Future.delayed(Duration(seconds: 2),(){
+      return "sw engineer";
+     });
+
+
+
+     print("$username - $bio");
+   }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -20,7 +45,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen'),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter+=1;
+          });
+        },
+        child: Text("counter is : $counter"),
+      ),
     );
   }
 }
