50801081cc7fd19130c421b409fb8597f5e7eb48
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-07-27 17:39:39 +0300

    Loaders Spinners

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -34,9 +35,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body :Padding(
-        padding: EdgeInsets.all(50.0),
-        child:Text("Loading"),
+      backgroundColor: Colors.blue[900],
+      body : Center(
+        child:SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        )
       )
     );
   }
